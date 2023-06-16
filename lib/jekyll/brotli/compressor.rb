caacf58cd42f63599f4c5bc36ce30cb405ec1d08
lsb <leebutterman@gmail.com>
2023-06-15 23:46:20 -0700

    Update compressor.rb

--- lib/jekyll/brotli/compressor.rb
+++ lib/jekyll/brotli/compressor.rb
@@ -25,7 +25,7 @@ module Jekyll
       #
       # @return void
       def self.compress_site(site)
-        ::Parallel.each(site.each_site_file.to_a) do |file|
+        ::Parallel.each(site.enum_for(:each_site_file)) do |file|
           next unless regenerate? file.destination(site.dest), site
 
           compress_file(
